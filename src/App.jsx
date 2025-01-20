0f53000aebe263be975fc7b9df0782a464ad4c77
Suhaib <enigma0080@gmail.com>
2025-01-20 10:57:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='display: flex; justify-content: center; align-items: center; height: 100vh; width: 100%;'>
+    <div className='display: flex; justify-content: center; align-items: center; height: 100vh; width: 100%; text-center;'>
       <h1>This is Auto Deployed app using GitHub Actions</h1>
     </div>
   )
